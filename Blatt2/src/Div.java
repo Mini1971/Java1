782ff1db72815fd5f3ba529a3cfafeab7afa01f6
heitel <joachim@heitel.name>
2014-12-31 13:12:41 +0100

    Blatt2

---
+++ Blatt2/src/Div.java
@@ -0,0 +1,19 @@
+
+public class Div {
+	public static void main(String[] args) {
+		int z1 = 10;
+		int z2 = 897;
+		
+		int s = z1/z2;
+		
+		System.out.print(s);
+		System.out.print(",");
+		
+		for (int i = 0; i < 60; i++) {
+			z1 = z1 * 10;
+			s = z1/z2;
+			System.out.print(s);
+			z1 = z1 % z2;
+		}
+	}
+}
