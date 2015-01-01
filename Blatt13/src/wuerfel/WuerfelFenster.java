162ae25c0f3395ed75bfaec5bdaf4776f357c43e
heitel <joachim@heitel.name>
2015-01-01 12:48:42 +0100

    Organise Imports

--- Blatt13/src/wuerfel/WuerfelFenster.java
+++ Blatt13/src/wuerfel/WuerfelFenster.java
@@ -15,7 +15,6 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class WuerfelFenster extends JFrame implements ActionListener {
