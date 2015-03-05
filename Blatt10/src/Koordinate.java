307736ccaf1c7660f349efb1bcebcfa84aa8bed2
heitel <joachim@heitel.name>
2015-03-05 08:03:46 +0100

    Plattformunabhängiger Browseraufruf

--- Blatt10/src/Koordinate.java
+++ Blatt10/src/Koordinate.java
@@ -1,4 +1,7 @@
+import java.awt.Desktop;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Koordinate {
 	private double breite;
@@ -14,18 +17,18 @@ public class Koordinate {
 		laenge = convert(lg, lm, ls);
 	}
 
-	private double convert(int bg, int bm,  int bs) {
+	private double convert(int bg, int bm, int bs) {
 		return bg + bm / 60.0 + bs / 3600.0;
 	}
 
 	private int[] c2b(double x) {
 		int[] erg = new int[3];
-	
-		erg[0] = (int)x;// Grad
+
+		erg[0] = (int) x;// Grad
 		double tmp = Math.abs((x - erg[0]) * 60);
-		erg[1] = (int)tmp;// Minuten
-		erg[2] = (int)((tmp - erg[1]) * 60); // Sekunden
-	
+		erg[1] = (int) tmp;// Minuten
+		erg[2] = (int) ((tmp - erg[1]) * 60); // Sekunden
+
 		return erg;
 	}
 
@@ -36,15 +39,19 @@ public class Koordinate {
 		erg += String.format("%d %d' %d'' ", tmp[0], tmp[1], tmp[2]);
 		tmp = c2b(laenge);
 		erg += String.format("%d %d' %d'' ", tmp[0], tmp[1], tmp[2]);
-	
+
 		return erg;
 	}
-	
+
 	public void show() {
 		try {
-			String iexplore = "\"c:\\Program Files\\Internet Explorer\\iexplore.exe\"";
-			String url = "http://maps.google.de/maps?q=" + breite + "," + laenge;
-			Runtime.getRuntime().exec( iexplore + " " + url);
+			if (Desktop.isDesktopSupported()) {
+				URI uri = new URI("http://maps.google.de/maps?q=" + breite
+						+ "," + laenge);
+				Desktop.getDesktop().browse(uri);
+			}
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
